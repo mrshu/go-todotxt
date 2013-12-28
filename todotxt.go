aacd76a67b6afdca5265eba0b3046cd7cafd00e9
mr.Shu <mr@shu.io>
2013-12-28 12:31:26 +0100

    first version of todotxt.go

---
+++ todotxt.go
@@ -0,0 +1,13 @@
+package todotxt
+
+import (
+        "time"
+)
+
+type Task struct {
+        todo string
+        priority string
+        create_date time.Time
+        contexts []string
+        projects []string
+}
