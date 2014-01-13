35f7766f6d80331f54a0ac3f2060ba88f828cca9
mr.Shu <mr@shu.io>
2014-01-13 23:00:24 +0100

    nonexistent file test

--- todotxt_test.go
+++ todotxt_test.go
@@ -9,3 +9,16 @@ func TestLoadTaskList (t *testing.T) {
                           tasklist.Len(), 8, tasklist)
         }
 }
+
+func TestLoadTaskListNonExistent (t *testing.T) {
+        defer func(){
+                if r:=recover(); r!=nil {
+                        // recovered
+                } else {
+                        t.Errorf("Something went seriously wrong")
+                }
+        }()
+        tasklist := LoadTaskList("nonexistent-file.txt")
+
+        t.Errorf("Something is still wrong %v", tasklist)
+}
