f8ccba1e16ce6c261c2b77cb947adc20eb9ca2d1
mr.Shu <mr@shu.io>
2014-01-13 22:40:20 +0100

    fixed test

--- todotxt_test.go
+++ todotxt_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestLoadTaskList (t *testing.T) {
         tasklist := LoadTaskList("todo.txt")
-        if tasklist.Count() != 7 {
-                t.Errorf("Something went wrong with LoadTaskList: is %v, want %v, list %v",
-                          tasklist.Count(), 7, tasklist)
+        if tasklist.Len() != 8 {
+                t.Errorf("Something went wrong with LoadTaskList: is %v, want %v\n%v",
+                          tasklist.Len(), 8, tasklist)
         }
 }
