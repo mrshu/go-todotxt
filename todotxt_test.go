91970407f2f45941cb667eeae14eea514914c842
mr.Shu <mr@shu.io>
2014-01-15 13:24:37 +0100

    priority test

--- todotxt_test.go
+++ todotxt_test.go
@@ -26,6 +26,10 @@ func TestLoadTaskListNonExistent (t *testing.T) {
 func TestCreateTask (t *testing.T) {
         task := CreateTask(1, "(A) +funny task with prioity and project")
         if task.id != 1 {
-                t.Errorf("Taskid: is %v, want %v", task.id, 1)
+                t.Errorf("id: is %v, want %v", task.id, 1)
+        }
+
+        if task.prioity != 'A' {
+                t.Errorf("priority: is %v, want %v", task.priority, 1)
         }
 }
